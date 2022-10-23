24a4bcfdad2dddf3aad0322efbea95df26519b55
Shreyansh Agarwal <shreyanshagarwal.16.2022@gmail.com>
2022-10-23 22:06:17 +0530

    resolved malfunction in application

--- base/api/urls.py
+++ base/api/urls.py
@@ -9,7 +9,6 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('', views.getDetails),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
