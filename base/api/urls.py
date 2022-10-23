ed47eda75c0a76302d85a38a20abffe275788cc2
Shreyansh Agarwal <shreyanshagarwal.16.2022@gmail.com>
2022-10-23 20:46:50 +0530

    completed login api

--- base/api/urls.py
+++ base/api/urls.py
@@ -1,16 +1,15 @@
 import imp
 from django.urls import path
 from . import views
+from .views import MyTokenObtainPairView
 
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
+    # TokenObtainPairView,
     TokenRefreshView,
 )
 
 urlpatterns = [
     path('', views.getDetails),
-    
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
 ]
