739adde79c3a28fed72d45be191631849a8cdf42
suhaillahmad <suhaillahmadd0@gmail.com>
2022-10-26 23:55:37 +0530

    bug fix env file

--- skillEdge/settings.py
+++ skillEdge/settings.py
@@ -182,8 +182,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'skilledge.contactus@gmail.com'
+EMAIL_HOST_PASSWORD = 'hzpgjxhybnwcemgm'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 CSRF_TRUSTED_ORIGINS = [
