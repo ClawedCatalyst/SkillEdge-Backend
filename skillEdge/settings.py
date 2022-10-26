db2e9e2acc89d0898d6e6b828bdd39adb2839db5
suhaillahmad <suhaillahmadd0@gmail.com>
2022-10-26 23:52:54 +0530

    bug fix - smtp error

--- skillEdge/settings.py
+++ skillEdge/settings.py
@@ -184,6 +184,7 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 CSRF_TRUSTED_ORIGINS = [
     'https://skilledge.herokuapp.com'
