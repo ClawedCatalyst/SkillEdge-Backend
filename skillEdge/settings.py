c6e7aa0414fe256cf8384789a2a10212ee360a3f
suhaillahmad <suhaillahmadd0@gmail.com>
2022-10-29 23:21:18 +0530

    Updated requirements.txt file

--- skillEdge/settings.py
+++ skillEdge/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 from datetime import timedelta
 import os
+import cloudinary_storage
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
