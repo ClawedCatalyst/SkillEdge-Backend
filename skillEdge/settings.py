f2b510114064ed90e37f5eec6b1537bfd634c51d
suhaillahmad <suhaillahmadd0@gmail.com>
2022-10-31 02:02:44 +0530

    Database: shifted from sqlite to postgresql

--- skillEdge/settings.py
+++ skillEdge/settings.py
@@ -136,12 +136,24 @@ WSGI_APPLICATION = 'skillEdge.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': os.environ.get('DATABASE_NAME'),
+       'USER': os.environ.get('DATABASE_USER'),
+       'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+       'HOST': 'ec2-44-195-162-77.compute-1.amazonaws.com',
+       'PORT': '5432'
+   }
 }
+
 
 
 # Password validation
