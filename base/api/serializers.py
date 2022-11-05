708807b2939054d807a6f59eb78b00e6b5b63e8d
suhaillahmad <suhaillahmadd0@gmail.com>
2022-11-05 20:13:36 +0530

    Bug Fix: Profile Serializer

--- base/api/serializers.py
+++ base/api/serializers.py
@@ -34,7 +34,7 @@ class loginSerializer(ModelSerializer):
 class profileSerializer(ModelSerializer):
     class Meta:
         model = NewUserRegistration
-        fields = ("name", "user_name" ,"picture", "gender","dateOfBirth","mobile","is_educator","interested")
+        fields = ("name", "user_name" ,"picture", "gender","dateOfBirth","mobile","is_educator")
     
 
 class otpcheckserializer(serializers.Serializer):
