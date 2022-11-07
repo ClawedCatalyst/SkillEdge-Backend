73d70cf9199238b9fb1d8db95fada3adb5dfc4bc
Shreyansh Agarwal <shreyanshagarwal.16.2022@gmail.com>
2022-11-07 19:17:22 +0530

    bug fixed : interest view in profile

--- base/api/serializers.py
+++ base/api/serializers.py
@@ -34,7 +34,7 @@ class loginSerializer(ModelSerializer):
 class profileSerializer(ModelSerializer):
     class Meta:
         model = NewUserRegistration
-        fields = ("name", "user_name" ,"picture", "gender","dateOfBirth","mobile","is_educator")
+        fields = ("name", "user_name" ,"picture", "gender","dateOfBirth","mobile","is_educator","email","interested")
     
 
 class otpcheckserializer(serializers.Serializer):
