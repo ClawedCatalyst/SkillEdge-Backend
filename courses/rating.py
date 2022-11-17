4836266837a7f090025f377c3f0643cb2a6e48b9
Shreyansh Agarwal <shreyanshagarwal.16.2022@gmail.com>
2022-11-17 13:14:45 +0530

    added live updation of course weighted rating

--- courses/rating.py
+++ courses/rating.py
@@ -1,17 +1,28 @@
 from courses.models import *
 
 def calculate_weighted_rating(course):
-    votes = course.review_count
-    print(votes)
-    avg_rating = course.rating
-    print(avg_rating)
+    specific_votes = course.review_count
+    # print(votes)
+    specific_avg_rating = course.rating
+    # print(avg_rating)
     all_courses = Course.objects.all()
     total_rating = 0.0
     for each_course in all_courses:
         total_rating += each_course.rating
     default_votes = 50
-    print(total_rating)
+    # print(total_rating)
     cummulative_rating = total_rating/len(all_courses)
-    print(cummulative_rating)
-    weighted_rating = ((avg_rating*votes)+(cummulative_rating*default_votes))/(votes + default_votes)
-    return weighted_rating
+    # print(cummulative_rating)
+    for hosted_course in all_courses:
+        votes = hosted_course.review_count
+        print(votes)
+        avg_rating = hosted_course.rating
+        print(avg_rating)
+        live_weighted_rating = ((avg_rating*votes)+(cummulative_rating*default_votes))/(votes + default_votes)
+        print(live_weighted_rating)
+        hosted_course.weighted_rating = live_weighted_rating
+        print(hosted_course.weighted_rating)
+        hosted_course.save()
+        print("\n")
+    specific_weighted_rating = ((specific_avg_rating*specific_votes)+(cummulative_rating*default_votes))/(specific_votes + default_votes)
+    return specific_weighted_rating 
